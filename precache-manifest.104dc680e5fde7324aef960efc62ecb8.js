2191e36aeabbc6182fe56ab5bf97e08032522e6d
Suyash Vardhan Mathur <suyash.mathur@research.iiit.ac.in>
2021-06-26 16:50:09 +0530

    Updates

---
+++ precache-manifest.104dc680e5fde7324aef960efc62ecb8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104a3cb4f395844833e6d77d4dc1276a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94df24ab57504ae96914",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3b0e42b9087ef01d2ee9",
+    "url": "/static/js/2.697a6ba3.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.697a6ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94df24ab57504ae96914",
+    "url": "/static/js/main.73b729a6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "16f56c92c4f635a171e7988640aac1cb",
+    "url": "/static/media/iiit-new.16f56c92.jpg"
+  },
+  {
+    "revision": "8599fbf2166056df0d85f426655f2b0d",
+    "url": "/static/media/resume.8599fbf2.pdf"
+  },
+  {
+    "revision": "f20cc82932545d73917df90b3aadf654",
+    "url": "/static/media/suyash.f20cc829.jpeg"
+  }
+]);
